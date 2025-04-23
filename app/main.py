0daa351dbf6d685b34526575fa64704e3190e988
seonb2n <626ksb@gmail.com>
2025-04-23 20:16:09 +0900

    response to multiple pings

--- app/main.py
+++ app/main.py
@@ -6,7 +6,10 @@ def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     connection, _ = server_socket.accept()
-    connection.sendall(b"+PONG\r\n")
+
+    while True:
+        connection.recv(1024)
+        connection.send(b"+PONG\r\n")
 
 if __name__ == "__main__":
     main()
